c7f3837a09a574c4fbcabaad439376ca4750be30
Olga Aukhadieva <sarole4ka@gmail.com>
2024-02-12 21:03:37 +0500

    Added try/except

--- src/video.py
+++ src/video.py
@@ -14,16 +14,22 @@ class Video:
     def __init__(self, video_id):
         """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
         self.video_id = video_id
-        self.video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                               id=video_id).execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.url: str = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,'
+                                                                        'topicDetails', id=self.video_id).execute()
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+            self.url: str = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         """Возвращает строковое представление объекта в виде названия видео."""
-        return self.video_title
+        return self.title
 
     @classmethod
     def get_service(cls):
